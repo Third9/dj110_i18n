ba856db312e2477030b6b0d146eae414a808756b
dev.third9 <dev.third9@gmail.com>
2017-02-14 17:00:15 +0900

    -. JavaScript i18n 구현

--- dj110_i18n/urls.py
+++ dj110_i18n/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.i18n import JavaScriptCatalog
 from django.conf.urls.i18n import i18n_patterns
 from myapp.views import MyApp
+
+js_info_dict = {
+    'packages': ('dj110_i18n')
+}
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -10,4 +15,5 @@ urlpatterns = [
 
 urlpatterns += i18n_patterns(
     url(r'^$', MyApp.as_view(), name='home'),
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(**js_info_dict), name='javascript-catalog'),
 )
